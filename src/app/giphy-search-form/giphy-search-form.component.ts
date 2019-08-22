a47f5ee9f44390d6f93db16518414e0c1dd916a2
michaelbrin <michaelbrin4870@gmail.com>
2019-08-22 23:50:05 +0300

    Emit search event to parent component

--- src/app/giphy-search-form/giphy-search-form.component.ts
+++ src/app/giphy-search-form/giphy-search-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-giphy-search-form',
@@ -6,9 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./giphy-search-form.component.css']
 })
 export class GiphySearchFormComponent implements OnInit {
-
+  @Output() emitSearch = new EventEmitter<any>()
+  
   searchTerm:string;
   constructor() { }
+
+  search(){
+    this.emitSearch.emit(this.searchTerm);
+  }
 
   ngOnInit() {
   }

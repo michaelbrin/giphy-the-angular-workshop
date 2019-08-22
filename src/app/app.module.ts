4b3125124b3d466dc0805ebd8866e9ea53eb807c
michaelbrin <michaelbrin4870@gmail.com>
2019-08-22 22:44:13 +0300

    Created the parent component  GiphyComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GiphyComponent } from './giphy/giphy.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GiphyComponent
   ],
   imports: [
     BrowserModule,
